a6c6ef9ae0da73e1fc8c8fa8818b477e4a013e28
mr-programmer90 <157228810+mr-programmer90@users.noreply.github.com>
2024-02-03 20:15:14 +0300

    Add files via upload
    switch case ile hesap makinesi c++

---
+++ Kaynak1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	char simge;
+	float s1, s2, sonuc;
+	cout << "Birinci sayiyi girin: ";
+	cin >> s1;
+	cout << "ikinci sayiyi girin: ";
+	cin >> s2;
+	cout << "Yapmak istediginiz islemi girin(+,-,*,/) ";
+	cin >> simge;
+
+	switch (simge) {
+
+	case '+':
+		sonuc = s1 + s2;
+		cout << sonuc;
+		break;
+
+	case '-':
+		sonuc = s1 - s2;
+		cout << sonuc;
+		break;
+
+	case '*':
+		sonuc = s1 * s2;
+		cout << sonuc;
+		break;
+
+	case '/':
+		sonuc = s1 / s2;
+		cout << sonuc;
+		break;
+
+	default:
+		cout << "Hatali giris";
+	}
+
+
+	return 0;
+}
